cf7cea59860fc26321749d1dc47e5d38cd4f257c
salekin6302 <salekin6302@gmail.com>
2022-01-21 13:32:04 +0000

    message modified
    git-svn-id: https://svn.riouxsvn.com/repo6302/trunk/test@3 277e12ef-49b3-4607-b050-eeed957e44e5

--- src/de/proetcon/Main.java
+++ src/de/proetcon/Main.java
@@ -3,6 +3,6 @@ package de.proetcon;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Salekin testing svn repository");
+	    System.out.println("Salekin testing svn repo");
     }
 }
