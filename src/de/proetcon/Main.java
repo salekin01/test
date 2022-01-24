60720fc4efcb3484b646d01581d07c91a7b8c71e
sap <salekin.prodhan@proetcon.de>
2022-01-24 09:34:00 +0100

    added .gitignore file and added messages in Main.java

--- src/de/proetcon/Main.java
+++ src/de/proetcon/Main.java
@@ -3,6 +3,7 @@ package de.proetcon;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Salekin testing svn repo");
+        System.out.println("Salekin testing svn repo");
+        System.out.println("Salekin testing github repo");
     }
 }
